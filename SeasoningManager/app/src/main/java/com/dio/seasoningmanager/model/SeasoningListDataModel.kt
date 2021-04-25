35988f0c2ba8f669edbc92854b331c68e481badd
DIO0550 <diot12345@yahoo.co.jp>
2021-04-25 23:35:33 +0900

    DB操作用のライブラリ追加。モデルクラス修正。

--- SeasoningManager/app/src/main/java/com/dio/seasoningmanager/model/SeasoningListDataModel.kt
+++ SeasoningManager/app/src/main/java/com/dio/seasoningmanager/model/SeasoningListDataModel.kt
@@ -1,8 +1,10 @@
 package com.dio.seasoningmanager.model
 
+import android.graphics.Bitmap
 import java.time.LocalDate
 
 class SeasoningListDataModel {
-    private val name: String = "";
+    private val name: String? = null;
     private val expirationDate: LocalDate? = null;
+    private val image: Bitmap? = null;
 }
